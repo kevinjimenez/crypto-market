e04ff9856824179ee41a8ca3eb919b55b8cc11e3
Kevin Jimenez <orlando.kj24@gmail.com>
2025-05-22 15:13:30 -0500

    feat: 🗑  delete comments

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -11,52 +11,6 @@ export class SeedService {
     private readonly http: AxiosAdapter,
     private readonly cryptoCurrencyService: CryptoCurrencyService,
   ) {}
-  // public async executeSeed() {
-  //   const cryptosToCreate: CreateCryptoCurrencyDto[] = [];
-  //   await this.cryptoCurrencyService.moveToHistory();
-
-  //   const data = await this.http.get<CryptoResponse[]>(
-  //     process.env.COIN_API ?? '',
-  //   );
-
-  //   for (const crypto of data) {
-  //     const lastCrypto = await this.cryptoCurrencyService.getLastCryptoCurrency(
-  //       crypto.id,
-  //     );
-
-  //     const newCrypto = new CreateCryptoCurrencyDto();
-  //     newCrypto.trend = lastCrypto?.trend ?? 'same';
-  //     newCrypto.high1h = lastCrypto?.high1h ?? crypto.current_price;
-  //     newCrypto.low1h = lastCrypto?.low1h ?? crypto.current_price;
-
-  //     if (lastCrypto && crypto.current_price === lastCrypto.currentPrice) {
-  //       newCrypto.trend = 'same';
-  //     } else if (lastCrypto && crypto.current_price > lastCrypto.currentPrice) {
-  //       newCrypto.trend = 'up';
-  //       newCrypto.high1h = crypto.current_price;
-  //     } else if (lastCrypto && crypto.current_price < lastCrypto.currentPrice) {
-  //       newCrypto.trend = 'down';
-  //       newCrypto.low1h = crypto.current_price;
-  //     }
-
-  //     newCrypto.name = crypto.name;
-  //     newCrypto.image = crypto.image;
-  //     newCrypto.symbol = crypto.symbol;
-  //     newCrypto.currentPrice = crypto.current_price;
-
-  //     const avg = (newCrypto.high1h + newCrypto.low1h) / 2;
-  //     newCrypto.avg = avg;
-
-  //     newCrypto.signal = crypto.current_price > avg ? 'B' : 'S';
-  //     newCrypto.tag = crypto.id;
-  //     newCrypto.isCurrent = true;
-  //     cryptosToCreate.push(newCrypto);
-  //   }
-
-  //   await this.cryptoCurrencyService.createMany(cryptosToCreate);
-
-  //   return 'Seed executed';
-  // }
 
   public async executeSeed() {
     const cryptosToCreate: CreateCryptoCurrencyDto[] = [];
@@ -121,7 +75,6 @@ export class SeedService {
     return 'Seed executed with simulated data';
   }
 
-  // @Cron('*/15 * * * * *')
   @Cron('*/12 * * * * *')
   async handleCron() {
     console.log('Running seed');
